9bdbfe5c8d1f71e499f3aa5653e6bae150fb120f
becivells <732903873@qq.com>
2021-09-28 14:55:30 +0800

    add flag

--- internal/reverse/reverse.go
+++ internal/reverse/reverse.go
@@ -126,5 +126,6 @@ func (reverse *Reverse) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		proxy.ServeHTTP(w, r)
 		return
 	}
+	w.Header().Set("GoblinServer", Version)
 	w.Write([]byte("403: Host forbidden"))
 }
